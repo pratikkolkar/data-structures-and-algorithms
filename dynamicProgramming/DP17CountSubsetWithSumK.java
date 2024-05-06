5f0fb19e50dc0c676c17429aebf6114deb844309
Pratikkumar Kolkar <pratikkolkar@gmail.com>
2024-05-06 12:17:52 +0530

    DP17 - base case to handle 0 at index 0

--- dynamicProgramming/DP17CountSubsetWithSumK.java
+++ dynamicProgramming/DP17CountSubsetWithSumK.java
@@ -2,19 +2,29 @@ package dynamicProgramming;
 
 import java.util.Arrays;
 
+/* 
+ Problem statement
+ You are given an array 'arr' of size 'n' containing positive integers and a target sum 'k'.
+
+ Find the number of ways of selecting the elements from the array such that the sum of chosen elements is equal to the target 'k'.
+
+ Since the number of ways can be very large, print it modulo 10 ^ 9 + 7.
+ */
 public class DP17CountSubsetWithSumK {
+    // set mod to 10 ^ 9 + 7
+    static int mod = (int) (Math.pow(10, 9) + 7);
+
     public static void main(String[] args) {
-        int[] arr = {1, 1, 4, 5};
-        int k =5;
+        int[] arr = { 0,1,2,3,4,0 };
+        int k = 4;
         // recursion(arr,k);
         // memoization(arr,k);
-        // tabulation(arr,k);
-        tabulationWithSpaceOptimized(arr,k);
+        // tabulation(arr, k);
+        tabulationWithSpaceOptimized(arr, k);
 
     }
 
-
-    /* 
+    /*
      * Approach pick and non-pick
      * 
      * TC: O(N * K)
@@ -22,27 +32,32 @@ public class DP17CountSubsetWithSumK {
      */
     private static void tabulationWithSpaceOptimized(int[] arr, int k) {
         int n = arr.length;
-        int[] dp=new int[k+1];
-        //Base case
-        dp[0]=1;
-        if(arr[0]<=k)
-            dp[arr[0]]=1;
-
-        for(int idx=1; idx<n; idx++){ 
-            int[] curr=new int[k+1];
-            curr[0]=1;
-            for (int sum = 1; sum <=k; sum++) {
-                curr[sum] += dp[sum];
-                if(sum>=arr[idx])
-                curr[sum]+=dp[sum-arr[idx]];
-            }
-            dp=curr;
+        int[] dp = new int[k + 1];
+        // Base case
+        if (arr[0] == 0) {
+            dp[0] = 2;
+        } else {
+            dp[0] = 1;
         }
-        System.out.println("Output: "+dp[k]);
+        if (arr[0] != 0 && arr[0] <= k)
+            dp[arr[0]] = 1;
+
+        for (int idx = 1; idx < n; idx++) {
+            int[] curr = new int[k + 1];
+            for (int sum = 0; sum <= k; sum++) {
+                int left = dp[sum];
+                int right = 0;
+                if (sum >= arr[idx])
+                    right = dp[sum - arr[idx]];
+
+                curr[sum] = (left + right) % mod;
+            }
+            dp = curr;
+        }
+        System.out.println("Output: " + dp[k]);
     }
 
-
-    /* 
+    /*
      * Approach pick and non-pick
      * 
      * TC: O(N * K)
@@ -50,77 +65,102 @@ public class DP17CountSubsetWithSumK {
      */
     private static void tabulation(int[] arr, int k) {
         int n = arr.length;
-        int[][] dp=new int[n][k+1];
-        //Base case
-        for (int i = 0; i < n; i++) {
-            dp[i][0]=1;
-        }
-        if(arr[0]<=k)
-            dp[0][arr[0]]=1;
+        int[][] dp = new int[n][k + 1];
+        // Base case
+        // for (int i = 0; i < n; i++) {
+        // dp[i][0] = 1;
+        // }
 
-        for(int idx=1; idx<n; idx++){
-            for (int sum = 1; sum <=k; sum++) {
-                dp[idx][sum] += dp[idx-1][sum];
-                if(sum>=arr[idx])
-                dp[idx][sum]+=dp[idx-1][sum-arr[idx]];
+        if (arr[0] == 0) {
+            dp[0][0] = 2;
+        } else {
+            dp[0][0] = 1;
+        }
+
+        if (arr[0] != 0 && arr[0] <= k)
+            dp[0][arr[0]] = 1;
+
+        for (int idx = 1; idx < n; idx++) {
+            for (int sum = 0; sum <= k; sum++) {
+                int left = dp[idx - 1][sum];
+                int right = 0;
+                if (sum >= arr[idx])
+                    right = dp[idx - 1][sum - arr[idx]];
+                dp[idx][sum] = (left + right) % mod;
             }
         }
-        System.out.println("Output: "+dp[n-1][k]);
+        System.out.println("Output: " + dp[n - 1][k]);
 
     }
 
     private static void memoization(int[] arr, int k) {
         int n = arr.length;
-        int[][] dp=new int[n][k+1];
-        for(int[] a:dp){
+        int[][] dp = new int[n][k + 1];
+        for (int[] a : dp) {
             Arrays.fill(a, -1);
         }
-        int output = memoization(arr,n-1, k,dp);
-        System.out.println("Output: "+output);
+        int output = memoization(arr, n - 1, k, dp);
+        System.out.println("Output: " + output);
     }
-    /* 
+
+    /*
      * Approach pick and non-pick
      * 
      * TC: O(N)
      * SC: O(N)+O(N)
      */
     private static int memoization(int[] arr, int idx, int k, int[][] dp) {
-        //Base case:
-        if(k==0) return 1;
-        if(idx==0) return arr[idx]==k?1:0;
+        // Base case:
+        if (idx == 0) {
+            if (k == 0 && arr[0] == 0)
+                return 2;
+            if (k == 0 || arr[0] == k)
+                return 1;
+            return 0;
+        }
 
-
-        if(dp[idx][k]!=-1) return dp[idx][k];
-        int left = recursion(arr, idx-1, k);
+        if (dp[idx][k] != -1)
+            return dp[idx][k];
+        int left = recursion(arr, idx - 1, k);
         int right = 0;
-        if(k>=arr[idx])
-            right = recursion(arr, idx-1, k-arr[idx]);
-        dp[idx][k]=left+right;
+        if (k >= arr[idx])
+            right = recursion(arr, idx - 1, k - arr[idx]);
+        dp[idx][k] = (left + right) % mod;
         return dp[idx][k];
     }
 
-    private static void recursion(int[] arr,int k) {
+    private static void recursion(int[] arr, int k) {
         int n = arr.length;
-        int output = recursion(arr,n-1, k);
-        System.out.println("Output: "+output);
+        int output = recursion(arr, n - 1, k);
+        System.out.println("Output: " + output);
     }
 
-
-    /* 
+    /*
      * Approach pick and non-pick
+     * 
+     * Edge Case: If we have value zero index 0, the base constion below will fail
+     *      if(target==0) return 1;
+     *      if(idx==0) return target==arr[0]?1:0;
+     * 
+     * We need to consider 2 subsets if 0 is present at index 0.
      * 
      * TC: O(2^n)
      * SC: O(N)+O(N)
      */
     private static int recursion(int[] arr, int idx, int k) {
-        //Base case:
-        if(k==0) return 1;
-        if(idx==0) return arr[idx]==k?1:0;
+        // Base case:
+        if (idx == 0) {
+            if (k == 0 && arr[0] == 0)
+                return 2;
+            if (k == 0 || arr[0] == k)
+                return 1;
+            return 0;
+        }
 
-        int left = recursion(arr, idx-1, k);
+        int left = recursion(arr, idx - 1, k);
         int right = 0;
-        if(k>=arr[idx])
-            right = recursion(arr, idx-1, k-arr[idx]);
-        return left+right;
+        if (k >= arr[idx])
+            right = recursion(arr, idx - 1, k - arr[idx]);
+        return (left + right) % mod;
     }
 }
